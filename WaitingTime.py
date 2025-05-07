207d5cb8163d342d70b07043e070b31d6b56dbc7
M ya <mya@Myas-Macbook.local>
2025-05-07 16:49:38 +0200

    complete API and connect them to website

---
+++ WaitingTime.py
@@ -0,0 +1,79 @@
+import requests
+
+class WaitingTimeClient:
+    RESIDENT_URL = 'https://secure1.info.gov.hk/immd/mobileapps/2bb9ae17/data/CPQueueTimeR.json'
+    VISITOR_URL  = 'https://secure1.info.gov.hk/immd/mobileapps/2bb9ae17/data/CPQueueTimeV.json'
+
+
+    def __init__(self):
+        self._resident_raw = None
+        self._visitor_raw  = None
+
+    def _fetch(self, url):
+        resp = requests.get(url)
+        resp.raise_for_status()
+        return resp.json()
+
+    def fetch_resident(self):
+        if self._resident_raw is None:
+            self._resident_raw = self._fetch(self.RESIDENT_URL)
+        return self._resident_raw
+
+    def fetch_visitor(self):
+        if self._visitor_raw is None:
+            self._visitor_raw = self._fetch(self.VISITOR_URL)
+        return self._visitor_raw
+
+    @staticmethod
+    def _judge_resident(time):
+        if time == 0:
+            return 'Less than 15 minutes'
+        elif time == 1:
+            return 'Less than 30 minutes'
+        elif time == 2:
+            return 'Less than 30 minutes'
+        elif time == 4:
+            return 'System Under Maintenance'
+        elif time == 99:
+            return 'Non Service Hours'
+
+    @staticmethod
+    def _judge_visitor(time):
+        if time == 0:
+            return 'Less than 30 minutes'
+        elif time == 1:
+            return 'Less than 45 minutes'
+        elif time == 2:
+            return '45 minutes or above'
+        elif time == 4:
+            return 'System Under Maintenance'
+        else:
+            return 'Non Service Hours'
+
+
+    def get_resident_times(self):
+        r_data = self.fetch_resident()
+        for place, info in r_data.items():
+            info['arrQueue'] = self._judge_resident(info['arrQueue'])
+            info['depQueue'] = self._judge_resident(info['depQueue'])
+        return r_data
+
+    def get_visitor_times(self):
+        v_data = self.fetch_visitor()
+        for place, info in v_data.items():
+            info['arrQueue'] = self._judge_visitor(info['arrQueue'])
+            info['depQueue'] = self._judge_visitor(info['depQueue'])
+        return v_data
+
+if __name__ == '__main__':
+    client = WaitingTimeClient()
+    resident = client.get_resident_times()
+    visitor  = client.get_visitor_times()
+
+    print("Residents:")
+    for place, times in resident.items():
+        print(f" - {place}: arrival {times['arrQueue']}, departure {times['depQueue']}")
+
+    print("\nVisitors:")
+    for place, times in visitor.items():
+        print(f" - {place}: arrival {times['arrQueue']}, departure {times['depQueue']}")
---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from WaitingTime import WaitingTimeClient
+
+app = Flask(__name__)
+client = WaitingTimeClient()   # instantiate once
+
+@app.route('/')
+def home():
+    resident_data = client.get_resident_times()
+    visitor_data  = client.get_visitor_times()
+    return render_template(
+        'index.html',
+        title='Home Page',
+        resident_data=resident_data,
+        visitor_data=visitor_data
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
