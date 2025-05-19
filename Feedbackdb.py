0b8c020172b97205ac6eeb7a5e5389753cf96b1b
M ya <mya@Myas-Macbook.local>
2025-05-19 22:36:00 +0200

    19/05 - Add the feedback part and create the feedback databse

---
+++ Feedbackdb.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Feedback(db.Model):
+    __tablename__ = 'feedbacks'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    point: Mapped[str] = mapped_column(nullable=False)
+    message: Mapped[str] = mapped_column(nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(default=datetime.utcnow, nullable=False)
--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect,request
 from WaitingTime import WaitingTimeClient
 import os
 from dotenv import load_dotenv
+from Feedbackdb import db, Feedback
 
-
+# Create the web application
 load_dotenv()
 app = Flask(__name__)
 api_key = os.environ.get("API_KEY")
+app.secret_key = "my_secret_key"
 client = WaitingTimeClient()   # instantiate once
+
+# Configure the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/feedback", methods=["POST"])
+def submit_feedback():
+    point = request.form.get("point")
+    message = request.form.get("message")
+
+    if not point or not message:
+        flash("Please complete all fields before submitting.", "error")
+    else:
+        new_feedback = Feedback(point=point, message=message)
+        db.session.add(new_feedback)
+        db.session.commit()
+        flash("Your feedback was submitted.", "success")
+
+    return redirect("/")  # or wherever your homepage is
 
 
 @app.route('/')
